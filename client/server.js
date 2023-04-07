d06854924d1650082096451d6983729e52643957
Sourabh Jyoti Das <95738675+SourabhJyotiDas@users.noreply.github.com>
2023-04-07 21:43:29 +0530

    Delete client directory

--- client/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const { connectToDatabase } = require("./config/database");
-
-connectToDatabase();
-
-
-app.listen(process.env.PORT,()=>{
-   console.log(`Server is working on port ${process.env.PORT}`);
-})
